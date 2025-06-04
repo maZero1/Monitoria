db9f881c7af64ad3f4ae78e89de86ec7ecd14356
maZero1 <tkmarlon594@gmail.com>
2025-06-03 21:30:36 -0300

    inicio do repositorio

--- src/Teste.java
+++ src/Teste.java
@@ -1,5 +1,8 @@
 public class Teste {
     public static void main(String[] args) {
         System.out.println("Teste de Repositorio agora");
+        System.out.println("duda gata");
+        System.out.println("GitHub ficou maluco");
+
     }
 }
